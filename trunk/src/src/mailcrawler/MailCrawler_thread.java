006a5dc1bed3336cf4d13364b1e9e1518af6c259
ivancad <ivancad@a22bead2-c56a-11de-9233-4169c94807ec>
2009-11-19 11:08:02 +0000

    Cambio del tag inicio al código principal

---
+++ trunk/src/src/mailcrawler/MailCrawler_thread.java
@@ -0,0 +1,30 @@
+package mailcrawler;
+
+import java.util.StringTokenizer;
+
+public class MailCrawler_thread extends Thread {
+
+	/**
+	 * @param args
+	 */
+	MailCrawler_thread(String url){
+	    
+	}
+	public void run(){
+	    
+	}
+	
+	/**
+	 * @param args [0] = nombre_fichero donde est‡n las direcciones de las webs de inicio.
+	 */
+	 
+	 //medotos implementados
+	 
+	 /*Llamada a GetURL para procesar enlaces*/
+	 
+	 //GetURL obtenerEnlaces=new GetURL(parametros);
+
+    
+
+
+}
